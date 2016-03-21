3f9a53446242074241aa94c07f1b8b4473255b5c
aiueoH@Cosair <aiueo.huang.github@gmail.com>
2016-03-21 16:21:16 +0800

    Remove useless code, file and fix parsing location.

--- background.js
+++ background.js
@@ -1,15 +1,8 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        // read `newIconPath` from request and read `tab.id` from sender
-        // chrome.browserAction.setIcon({
-        //     path: request.newIconPath,
-        //     tabId: sender.tab.id
-        // });
 		if (request.action != "updateIcon") return;
 		chrome.browserAction.setIcon({
 			path: request.newIconPath,
 			tabId: sender.tab.id
 		});
     });
-
-// chrome.browserAction.setIcon({path: "red.png"});
--- content.js
+++ content.js
@@ -1,6 +1,8 @@
-if ($("body").attr('id') == 'wwwRutenComTw') {
+function getInfo() {
+	var item_title = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > h2");
+	if (item_title.length == 0) return;
+
 	var reputation = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > div.intro-section.clearfix.auction-data > div.intro-section-right.product-purchase > div.item-purchase.customizable-borderless.purchase-section > div > div.item-info-owner.media-img-rev > p:nth-child(7) > a.text-medium > strong");
-	
 	var payment_method_1 = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > div.intro-section.clearfix.auction-data > div.intro-section-right.product-purchase > div.item-purchase.customizable-borderless.purchase-section > div > div.item-info-detail.media-body > table:nth-child(3) > tbody > tr > td:nth-child(2) > ul > li.sub0 > span")
 	var payment_method_2 = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > div.intro-section.clearfix.auction-data > div.intro-section-right.product-purchase > div.item-purchase.customizable-borderless.purchase-section > div > div.item-info-detail.media-body > table:nth-child(3) > tbody > tr > td:nth-child(2) > ul > li.sub1 > span")
 	var payment_method_3 = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > div.intro-section.clearfix.auction-data > div.intro-section-right.product-purchase > div.item-purchase.customizable-borderless.purchase-section > div > div.item-info-detail.media-body > table:nth-child(3) > tbody > tr > td:nth-child(2) > ul > li.sub2 > span")
@@ -8,22 +10,24 @@ if ($("body").attr('id') == 'wwwRutenComTw') {
 	var payment_method_5 = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > div.intro-section.clearfix.auction-data > div.intro-section-right.product-purchase > div.item-purchase.customizable-borderless.purchase-section > div > div.item-info-detail.media-body > table:nth-child(3) > tbody > tr > td:nth-child(2) > ul > li.sub4 > span")
 	var payment_method_6 = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > div.intro-section.clearfix.auction-data > div.intro-section-right.product-purchase > div.item-purchase.customizable-borderless.purchase-section > div > div.item-info-detail.media-body > table:nth-child(3) > tbody > tr > td:nth-child(2) > ul > li.sub5 > span")
 	var payment_method_7 = $("#wwwRutenComTw > div.rt-page > div.base-theme.rt-goods.rt-wrap > div > div.rt-grid-4-5.side-shadow.customizable-sideshadow > div > div.intro-section.clearfix.auction-data > div.intro-section-right.product-purchase > div.item-purchase.customizable-borderless.purchase-section > div > div.item-info-detail.media-body > table:nth-child(3) > tbody > tr > td:nth-child(2) > ul > li.sub6 > span")
-
 	var location = $("#product-memo > li.location > span")
-
-
-	alert(	"Reputation:" + reputation.html() + "\n" +
-			"Payment Method:\n"+ payment_method_1.html() + "\n" 
-						       + payment_method_2.html() + "\n"
-						       + payment_method_3.html() + "\n"
-						       + payment_method_4.html() + "\n"
-						       + payment_method_5.html() + "\n"
-						       + payment_method_6.html() + "\n"
-						       + payment_method_7.html() + "\n"
-
+	alert("Reputation:" + reputation.html() + "\n"
+		+ "\n"
+		+ "Payment Method:\n"+ payment_method_1.html() + "\n" 
+		+ payment_method_2.html() + "\n"
+		+ payment_method_3.html() + "\n"
+		+ payment_method_4.html() + "\n"
+		+ payment_method_5.html() + "\n"
+		+ payment_method_6.html() + "\n"
+		+ payment_method_7.html() + "\n"
+		+ "\n"
+		+ "location:\n"
+		+ location.html() + "\n"
 	);
 	chrome.runtime.sendMessage({
 		action : "updateIcon",
 		newIconPath : "red.png"
 	});
 }
+
+getInfo();
